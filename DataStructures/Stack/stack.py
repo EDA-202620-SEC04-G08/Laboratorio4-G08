989ef45b5a009706d5355a556a84052a6638fa87
jpenalozab <j.penalozab@uniandes.com>
2026-08-27 10:27:34 -0500

    stack versión 1 listo

--- DataStructures/Stack/stack.py
+++ DataStructures/Stack/stack.py
@@ -1,1 +1,31 @@
-import
+import DataStructures.List.single_linked_list as sll
+
+
+def new_stack():
+    return sll.new_list()
+
+
+def push(my_stack, element):
+    return sll.add_first(my_stack, element)
+
+
+def pop(my_stack):
+    if sll.is_empty(my_stack):
+        raise Exception('EmptyStructureError: stack is empty')
+    else:
+        return sll.remove_first(my_stack)
+
+
+def is_empty(my_stack):
+    return sll.is_empty(my_stack)
+
+
+def top(my_stack):
+    if sll.is_empty(my_stack):
+        raise Exception('EmptyStructureError: stack is empty')
+    else:
+        return sll.get_element(my_stack, 1)
+
+
+def size(my_stack):
+    return sll.size(my_stack)
