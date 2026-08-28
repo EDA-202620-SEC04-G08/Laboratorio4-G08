1b3228232b271eb0637d941f649cf4c0ab446033
Juan Pablo de Contreras <jp.contrerasg12@uniandes.edu.co>
2026-08-27 21:57:55 -0500

    Correcciones Queue

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -8,20 +8,21 @@ def enqueue(my_queue, element):
     lt.add_last(my_queue, element)
     return my_queue
 
-def dequeue(my_queue, element):
-    if my_queue["size"] > 0:
-        my_queue["elements"] = lt.delete_element(element)
+def dequeue(my_queue):
+    if lt.size(my_queue) > 0:
+        primer_elemento = lt.first_element(my_queue)
+        lt.delete_element(my_queue,0)
     else:
         raise Exception('EmptyStructureError: queue is empty')
-    return my_queue
+    return primer_elemento
 
 def is_empty(my_queue):
     vacio = lt.is_empty(my_queue)
     return vacio
 
 def peek (my_queue):
-    if my_queue["size"] > 0:
-        primer_elemento = lt.first_element[my_queue]
+    if lt.size(my_queue) > 0:
+        primer_elemento = lt.first_element(my_queue)
     else:
         raise Exception('EmptyStructureError: queue is empty')
     return primer_elemento
